ab490dbec4ea31e834cf4b5cb779c004df8f7269
Anton Smirnov <dev@antonsmirnov.name>
2014-03-13 21:10:21 +0600

    - fixed: crash on x86

--- lib/src/name/antonsmirnov/android/acra_breakpad/NativeExceptionHandler.java
+++ lib/src/name/antonsmirnov/android/acra_breakpad/NativeExceptionHandler.java
@@ -11,6 +11,7 @@ import java.io.File;
 public class NativeExceptionHandler {
 
     static {
+        System.loadLibrary("gnustl_shared");
         System.loadLibrary("native_exception_handler");
     }
 
